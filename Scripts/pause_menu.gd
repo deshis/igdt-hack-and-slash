758450409c0370ae0021739ec972109c7a0f7852
Altti Äyräs <55975611+deshis@users.noreply.github.com>
2025-09-28 17:30:54 +0300

    basic pause menu

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+func _ready() -> void:
+	visible = false
+
+func toggle_pause()->void:
+	visible = !visible
+	get_tree().paused = !get_tree().paused
+	
+	
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.is_action_pressed("ui_cancel"):
+			toggle_pause()
+
+
+func _on_continue_button_pressed() -> void:
+	toggle_pause()
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
