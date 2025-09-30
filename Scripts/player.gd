b394617aa07151dff04390b6cf0762370b1bb81b
unknown <jsponn@utu.fi>
2025-09-30 14:54:16 +0300

    add light attack and face mouse

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,6 +10,16 @@ var input: Vector2
 
 signal update_hp_bar
 signal dash_used
+signal light_attack_used
+
+
+var can_attack := true
+var attacking := false
+@onready var light_attack_cooldown_timer: Timer = $LightAttackCooldownTimer
+@onready var light_attack_length_timer: Timer = $LightAttackLengthTimer
+@onready var light_attack_sprite = $light_attack
+var light_attack_cooldown:= 0.2
+var light_attack_length:= 0.1
 
 var can_dash:= true
 var dashing = false
@@ -24,6 +34,7 @@ var current_speed:= movement_speed
 
 func _physics_process(delta) -> void:
 	update_input()
+	update_facing_dir()
 	
 	if Input.is_action_pressed("movement_ability") and can_dash and input.length() > 0:
 		can_dash = false
@@ -36,6 +47,13 @@ func _physics_process(delta) -> void:
 		velocity = lerp(velocity, input * current_speed, acceleration * delta)
 	
 	move_and_slide()
+	
+	if Input.is_action_pressed("light_attack") and can_attack:
+		can_attack = false
+		light_attack_length_timer.start(light_attack_length)
+		light_attack_cooldown_timer.start(light_attack_cooldown)
+		light_attack_used.emit(light_attack_cooldown)
+		light_attack()
 
 
 func update_input() -> void:
@@ -43,6 +61,27 @@ func update_input() -> void:
 		input.x = Input.get_axis("move_left", "move_right")
 		input.y = Input.get_axis("move_up", "move_down")
 	input = input.normalized()
+
+func update_facing_dir() -> void:
+	var screen_center: Vector2 = get_viewport_rect().size / 2
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	
+	var dir: Vector2 = mouse_pos - screen_center
+	rotation = dir.angle() + PI / 2
+
+
+func light_attack() -> void:
+	light_attack_sprite.visible = true
+	attacking = true
+
+
+func _on_light_attack_length_timer_timeout() -> void:
+	light_attack_sprite.visible = false
+	attacking = false
+
+
+func _on_light_attack_cooldown_timer_timeout() -> void:
+	can_attack = true
 
 
 func _on_dash_cooldown_timer_timeout() -> void:
