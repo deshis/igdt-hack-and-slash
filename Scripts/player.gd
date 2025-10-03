04f5b0ffc6a5431063c30005c75e96f68fd4c5f5
JaakkoPonnelin <jsponn@utu.fi>
2025-10-03 13:50:57 +0300

    add heavy attack

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,8 @@ class_name Player extends CharacterBody2D
 
 var input: Vector2
 
+var can_move := true
+var can_look_around := true
 @export var movement_speed := 500.0
 @export var acceleration := 15.0
 
@@ -10,16 +12,21 @@ var input: Vector2
 
 signal update_hp_bar
 signal dash_used
-signal light_attack_used
+signal attack_used
 
 
 var can_attack := true
 var attacking := false
-@onready var light_attack_cooldown_timer: Timer = $LightAttackCooldownTimer
-@onready var light_attack_length_timer: Timer = $LightAttackLengthTimer
-@onready var light_attack_sprite = $light_attack
+@onready var attack_cooldown_timer: Timer = $AttackCooldownTimer
+@onready var attack_length_timer: Timer = $AttackLengthTimer
+
+@onready var light_attack_object = $LightAttackObject
 var light_attack_cooldown:= 0.2
 var light_attack_length:= 0.1
+
+@onready var heavy_attack_object = $HeavyAttackObject
+var heavy_attack_cooldown:= 0.4
+var heavy_attack_length:= 0.3
 
 var can_dash:= true
 var dashing = false
@@ -34,26 +41,21 @@ var current_speed:= movement_speed
 
 func _physics_process(delta) -> void:
 	update_input()
-	update_facing_dir()
+	if can_look_around:
+		update_facing_dir()
 	
-	if Input.is_action_pressed("movement_ability") and can_dash and input.length() > 0:
-		can_dash = false
-		dashing = true
-		dash_length_timer.start(dash_length)
-		dash_cooldown_timer.start(dash_cooldown)
-		current_speed = dash_speed
-		dash_used.emit(dash_cooldown)
-	else:
+	if Input.is_action_pressed("movement_ability") and can_move and can_dash and input.length() > 0:
+		dash()
+	elif can_move:
 		velocity = lerp(velocity, input * current_speed, acceleration * delta)
 	
-	move_and_slide()
-	
 	if Input.is_action_pressed("light_attack") and can_attack:
-		can_attack = false
-		light_attack_length_timer.start(light_attack_length)
-		light_attack_cooldown_timer.start(light_attack_cooldown)
-		light_attack_used.emit(light_attack_cooldown)
 		light_attack()
+	
+	if Input.is_action_just_pressed("heavy_attack") and can_attack:
+		heavy_attack()
+	
+	move_and_slide()
 
 
 func update_input() -> void:
@@ -62,26 +64,59 @@ func update_input() -> void:
 		input.y = Input.get_axis("move_up", "move_down")
 	input = input.normalized()
 
+
 func update_facing_dir() -> void:
 	var screen_center: Vector2 = get_viewport_rect().size / 2
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
 	
 	var dir: Vector2 = mouse_pos - screen_center
-	rotation = dir.angle() + PI / 2
+	rotation = dir.angle()+ Vector2.DOWN.angle()
 
 
 func light_attack() -> void:
-	light_attack_sprite.visible = true
+	can_attack = false
+	light_attack_object.visible = true
+	attack_length_timer.start(light_attack_length)
+	attack_cooldown_timer.start(light_attack_cooldown)
+	attack_used.emit(light_attack_cooldown)
 	attacking = true
 
 
-func _on_light_attack_length_timer_timeout() -> void:
-	light_attack_sprite.visible = false
+func heavy_attack() -> void:
+	can_attack = false
+	can_move = false
+	can_look_around = false
+	
+	heavy_attack_object.visible = true
+	attack_length_timer.start(heavy_attack_length)
+	attack_cooldown_timer.start(heavy_attack_cooldown)
+	attack_used.emit(heavy_attack_cooldown)
+	attacking = true
+	
+	# TODO: small dash in the dir of the attack
+	velocity = Vector2(0,0)
+
+
+func dash():
+	can_dash = false
+	dashing = true
+	dash_length_timer.start(dash_length)
+	dash_cooldown_timer.start(dash_cooldown)
+	current_speed = dash_speed
+	dash_used.emit(dash_cooldown)
+
+
+func _on_attack_length_timer_timeout() -> void:
+	# TODO: need to make this smarter later, but for now it just disables both attacks
+	light_attack_object.visible = false
+	heavy_attack_object.visible = false
 	attacking = false
 
 
-func _on_light_attack_cooldown_timer_timeout() -> void:
+func _on_attack_cooldown_timer_timeout() -> void:
 	can_attack = true
+	can_move = true
+	can_look_around = true
 
 
 func _on_dash_cooldown_timer_timeout() -> void:
