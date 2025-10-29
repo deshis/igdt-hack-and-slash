654b42e7bfa3865c8e12831c86887b7b004d3508
sucary <78484823+sucary@users.noreply.github.com>
2025-10-29 16:37:37 +0200

    Add bgm

--- Scripts/globals/SoundManager.gd
+++ Scripts/globals/SoundManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 var sfx_players: Dictionary = {}
 var music_player: AudioStreamPlayer
+var bgm_players: Dictionary = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +10,23 @@ func _ready() -> void:
 	music_player = AudioStreamPlayer.new()
 	add_child(music_player)
 	
+	bgm_players = {
+		"common_bgm": load("res://Assets/audio/bgm.mp3")
+	}
+	
+	# TODO
+	# Hard-coded bgm_player, go with this before we have a proper level system and bgm for different levels.
+	music_player.stream = bgm_players["common_bgm"]
+	music_player.play()
+	
 	sfx_players = {
 		"light_attack": load("res://Assets/audio/effect_light_attack.wav"),
 		"heavy_attack": load(""), # TODO
 		"dash": load("res://Assets/audio/effect_dash.wav"),
 		"hit": load("res://Assets/audio/effect_hit.wav")
 	}
+	
+	
 
 # Play one-shot sound effects
 func play_sfx(name: String, position: Vector2 = Vector2.ZERO) -> void:
@@ -29,5 +41,3 @@ func play_sfx(name: String, position: Vector2 = Vector2.ZERO) -> void:
 	add_child(player)
 	player.play()
 	player.finished.connect(player.queue_free)
-
-# TODO: Background music plauer
