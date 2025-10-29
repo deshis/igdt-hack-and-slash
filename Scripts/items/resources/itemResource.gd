d2deaa9439c19d96d1c13836a4e0aa56f71e59d2
opnikk <nuupa4@gmail.com>
2025-10-30 01:56:17 +0200

    basic item system implementation

---
+++ Scripts/items/resources/itemResource.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name ItemResource
+
+@export var item_name: String
+@export var item_description: String
+@export var type: ItemType.Type
+@export var grade: ItemType.Grade
+@export var icon: Texture2D
+@export var effects: Array[Resource] = []
+
+var equipped: bool = false
