ac79b524cbda170a83fbb51ac6f51170fc363663
sucary <78484823+sucary@users.noreply.github.com>
2025-11-27 17:28:19 +0200

    Add heavy attack SFX and better pitch variation

--- Scripts/globals/SoundManager.gd
+++ Scripts/globals/SoundManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var sfx_players: Dictionary = {}
+var sfx_pitch_ranges: Dictionary = {}
 var music_player: AudioStreamPlayer
 var bgm_players: Dictionary = {}
 
@@ -17,8 +18,7 @@ func _ready() -> void:
 		
 	}
 	
-	# TODO
-	# Hard-coded bgm_player, go with this before we have a proper level system and bgm for different levels.
+	# TODO Hard-coded bgm_player, go with this before we have a proper level system and bgm for different levels.
 	music_player.stream = bgm_players["common_bgm"]
 	music_player.play()
 	
@@ -26,15 +26,23 @@ func _ready() -> void:
 	# SFX part
 	sfx_players = {
 		"light_attack": load("res://Assets/audio/effect_light_attack.wav"),
-		# TODO Create own SFX for heavy_Attack
-		"heavy_attack": load("res://Assets/audio/effect_light_attack.wav"), 
+		"heavy_attack": load("res://Assets/audio/effect_heavy_attack.wav"), 
 		"dash": load("res://Assets/audio/effect_dash.wav"),
 		"hit": load("res://Assets/audio/effect_hit.wav"),
 		"enemy_die": load("res://Assets/audio/effect_enemy_die.wav"),
-		"dot_sfx": load("res://Assets/audio/effect_dot.wav")
+		"dot_sfx": load("res://Assets/audio/effect_dot.wav"),
+		"heal":load("res://Assets/audio/effect_heal.wav")
 	}
 	
-	
+	sfx_pitch_ranges = {
+		"light_attack": [0.5, 1.5],
+		"heavy_attack": [0.9, 1.2],
+		"dash": [0.5, 1.5],
+		"hit": [0.5, 1.2],
+		"enemy_die": [0.5, 1.5],
+		"dot_sfx": [0.5, 1.5],
+		"heal": [0.5, 1.0]
+	}
 
 # Play one-shot sound effects
 func play_sfx(name: String, position: Vector2 = Vector2.ZERO) -> void:
@@ -49,11 +57,11 @@ func play_sfx(name: String, position: Vector2 = Vector2.ZERO) -> void:
 	
 	player.bus = "SFX"
 	
-	# Pitch variation, currently applies to all SFX
-	var min_pitch := 0.5
-	var max_pitch := 1.5
-	player.pitch_scale = randf_range(min_pitch, max_pitch)
+	# Pitch variation
+	var pitch_range = sfx_pitch_ranges.get(name, [0.9, 1.1])
+	player.pitch_scale = randf_range(pitch_range[0], pitch_range[1])
 	
 	add_child(player)
 	player.play()
+	
 	player.finished.connect(player.queue_free)
