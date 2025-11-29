f4d96a0b67b97f10650952538334b5520b8dc5c2
Eversti <eevertti02@gmail.com>
2025-11-29 13:13:01 +0200

    Slasher model and animations added

--- Scripts/enemy/slasher.gd
+++ Scripts/enemy/slasher.gd
@@ -7,7 +7,19 @@ var face_player = false
 @export var dash_speed = 1000
 var is_dashing = false
 
+@onready var animator = $"SubViewport/3DView/slasher/AnimationPlayer"
+@onready var model_view = $"SubViewport/3DView"
+@onready var camera_point = $"SubViewport/3DView/slasher"
+@onready var slasher_sprite = $Sprite2D
+
+func _ready() -> void:
+	model_view.position = Vector3(randf()*1e6, randf()*1e6, randf()*1e6)
+	animator.animation_finished.connect(_on_anim_finished)
+
 func _physics_process(delta: float) -> void:
+	slasher_sprite.rotation = -rotation
+	camera_point.rotation.y = -deg_to_rad(round(rad_to_deg(rotation+PI) / 5.0) * 5.0)
+	
 	if not player or not target_provider:
 		return
 	
@@ -26,18 +38,17 @@ func perform_attack() -> void:
 	attack_area_hitbox.disabled = false
 	is_dashing = true
 	current_speed = dash_speed
-	
-	attack_length_timer.start()
+
 
 func face_towards_player(delta: float) -> void:
 	var dir = (player.global_position - global_transform.origin).normalized()
 	update_facing_dir(delta, dir)
 
-
 func _on_navigation_agent_2d_target_reached() -> void:
 	target_provider = TargetSelf.new()
 	face_player = true
 	face_player_length_timer.start()
+	animator.play("Attack")
 
 func _on_face_player_length_timer_timeout() -> void:
 	face_player = false
@@ -47,3 +58,9 @@ func _on_attack_length_timer_timeout() -> void:
 	super._on_attack_length_timer_timeout()
 	is_dashing = false
 	current_speed = enemy.speed
+	animator.play("Walk")
+
+func _on_anim_finished(anim_name):
+	match anim_name:
+		'Attack':
+			_on_attack_length_timer_timeout()
