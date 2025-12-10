01fda1b008230a0d4d0e1cf29ff2d5bd63e18705
Eversti <eevertti02@gmail.com>
2025-12-10 16:02:35 +0200

    Player weapon mesh and trail fix

--- Scripts/globals/ItemGlobals.gd
+++ Scripts/globals/ItemGlobals.gd
@@ -3,12 +3,12 @@ extends Node
 #Global values for distinguishing between primary & secondary attacks
 #Accessed via ItemGlobals.
 
-var primary_weapon_type = "?"
-var primary_attack_type = "?"
+var primary_weapon_type = "Default"
+var primary_attack_type = "Default"
 var primary_weapon_mesh = null
 
-var secondary_weapon_type = "?"
-var secondary_attack_type = "?"
+var secondary_weapon_type = "Default"
+var secondary_attack_type = "Default"
 var secondary_weapon_mesh = null
 
 var primary: bool = false
