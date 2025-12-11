9e2a2d7d74c2504a1fbac55474f868123738ed07
JaakkoPonnelin <jsponn@utu.fi>
2025-12-11 20:39:42 +0200

    add interact prompt to door

--- Scripts/area_load_next.gd
+++ Scripts/area_load_next.gd
@@ -1,17 +1,36 @@
 extends Node3D
-var unlocked:bool =true
 
+@export var label: Label3D
 
+var player = GameManager.player
+var player_on_portal := false
+
+func _ready() -> void:
+	if not label:
+		return
+	
+	var interact = InputMap.action_get_events("interact")
+	var button_name = interact[0].as_text()
+	label.text = button_name + " interact"
+
+func _physics_process(_delta: float) -> void:
+	if not player or not label:
+		return
+	
+	var dist = global_position.distance_to(player.global_position)
+	if dist < 3.0:
+		label.visible = true
+	else:
+		label.visible = false
+	
+	if Input.is_action_just_pressed("interact") and player_on_portal:
+		GameManager.load_next_stage()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
-		# add prompt?
-		if unlocked==true:
-			#Maybe signal a key to press not just this?
-			GameManager.load_next_stage()
-		else:
-			pass # Play sound or something?
-		
+		player_on_portal = true
+
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	pass # Replace with function body.
+	if body.name == "Player":
+		player_on_portal = false
